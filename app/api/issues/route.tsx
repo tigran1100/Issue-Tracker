811ea181a3ea49ed15e886ab3c3fd01d88da3304
Tigran <tigran.balayan.y@gmail.com>
2024-02-11 18:13:19 +0400

    Created POST Api for creating issues

--- app/api/issues/route.tsx
+++ app/api/issues/route.tsx
@@ -1,5 +1,11 @@
+// NextJS
 import { NextRequest, NextResponse } from "next/server";
+
+// Prisma
 import prisma from "@/prisma/client";
+
+// Zod validations
+import { schema_issue_create_validation } from "@/validations";
 
 export async function GET(request: NextRequest) {
 	const issues = await prisma.issue.findMany({
@@ -28,6 +34,65 @@ export async function GET(request: NextRequest) {
 				data: { request: request },
 			},
 			{ status: 404 }
+		);
+	}
+}
+
+export async function POST(request: NextRequest) {
+	if (
+		!request.headers.get("Content-Length") ||
+		request.headers.get("Content-Length") === "0"
+	) {
+		return NextResponse.json(
+			{
+				success: 0,
+				reason: "Request body is required",
+				data: { request: request },
+			},
+			{ status: 400 }
+		);
+	}
+
+	let body = await request.json();
+	let validation = schema_issue_create_validation.safeParse(body);
+
+	if (!validation.success) {
+		return NextResponse.json(
+			{
+				success: 0,
+				reason: validation.error.format(),
+				data: { body: body },
+			},
+			{ status: 400 }
+		);
+	}
+
+	const prisma_create_request = await prisma.issue.create({
+		data: {
+			title: body.title,
+			description: body.description,
+		},
+	});
+
+	if (prisma_create_request) {
+		return NextResponse.json(
+			{
+				success: 1,
+				reason: "1",
+				data: {
+					body: body,
+				},
+			},
+			{ status: 201 }
+		);
+	} else {
+		return NextResponse.json(
+			{
+				success: 0,
+				reason: "Something went wrong, contact the developer for more details",
+				data: { body: body },
+			},
+			{ status: 201 }
 		);
 	}
 }
