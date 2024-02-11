3d931586ab1ba89815be25e824062d2108e701a4
Tigran <tigran.balayan.y@gmail.com>
2024-02-11 22:39:02 +0400

    Created PATCH Api for editing single error

--- app/api/issues/route.tsx
+++ app/api/issues/route.tsx
@@ -5,7 +5,10 @@ import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/prisma/client";
 
 // Zod validations
-import { schema_issue_create_validation } from "@/validations";
+import {
+	schema_issue_create_validation,
+	schema_issue_patch_validation,
+} from "@/validations";
 
 export async function GET(request: NextRequest) {
 	const issues = await prisma.issue.findMany({
@@ -82,6 +85,68 @@ export async function POST(request: NextRequest) {
 				data: {
 					body: body,
 					request: prisma_create_request,
+				},
+			},
+			{ status: 201 }
+		);
+	} else {
+		return NextResponse.json(
+			{
+				success: 0,
+				reason: "Something went wrong, contact the developer for more details",
+				data: { body: body },
+			},
+			{ status: 201 }
+		);
+	}
+}
+
+export async function PATCH(request: NextRequest) {
+	if (
+		!request.headers.get("Content-Length") ||
+		request.headers.get("Content-Length") === "0"
+	) {
+		return NextResponse.json(
+			{
+				success: 0,
+				reason: "Request body is required",
+				data: { request: request },
+			},
+			{ status: 400 }
+		);
+	}
+
+	let body = await request.json();
+	let validation = schema_issue_patch_validation.safeParse(body);
+
+	if (!validation.success) {
+		return NextResponse.json(
+			{
+				success: 0,
+				reason: validation.error.format(),
+				data: { body: body },
+			},
+			{ status: 400 }
+		);
+	}
+
+	const prisma_create_request = await prisma.issue.update({
+		where: {
+			id: body.id,
+		},
+		data: {
+			title: body.title,
+			description: body.description,
+		},
+	});
+
+	if (prisma_create_request) {
+		return NextResponse.json(
+			{
+				success: 1,
+				reason: "1",
+				data: {
+					body: body,
 				},
 			},
 			{ status: 201 }
