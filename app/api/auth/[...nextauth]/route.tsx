525e2f8edb5755c58758b3c698f681799734995f
Tigran <tigran.balayan.y@gmail.com>
2024-02-13 20:38:42 +0400

    Improved NextAuth

--- app/api/auth/[...nextauth]/route.tsx
+++ app/api/auth/[...nextauth]/route.tsx
@@ -4,14 +4,26 @@ import type { NextApiRequest, NextApiResponse } from "next";
 // NextAuth
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
 
-const handler = NextAuth({
+// Prisma
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export const auth_options = {
+	adapter: PrismaAdapter(prisma),
 	providers: [
 		GoogleProvider({
 			clientId: process.env.GOOGLE_CLIENT_ID as string,
 			clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
 		}),
 	],
-});
+	session: {
+		strategy: "jwt",
+	},
+} as any;
+
+const handler = NextAuth(auth_options);
 
 export { handler as GET, handler as POST };
