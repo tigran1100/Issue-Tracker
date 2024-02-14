4637224a337bfb77cad7597941672891294a809a
Tigran <tigran.balayan.y@gmail.com>
2024-02-14 14:40:42 +0400

    Refactored the navbar

--- app/components/navbar/navbar.tsx
+++ app/components/navbar/navbar.tsx
@@ -14,9 +14,26 @@ import { Avatar, Button, DropdownMenu, Text } from "@radix-ui/themes";
 import "./styles.css";
 
 const Navbar = () => {
+	return (
+		<>
+			<div className="c-navbar">
+				<div className="c-navbar-inner page_content">
+					<Link href="/" className="c-navbar-left">
+						<FaBug />
+					</Link>
+					<div className="c-navbar-right">
+						<Navbar_links />
+						<Dropdown_menu />
+					</div>
+				</div>
+			</div>
+		</>
+	);
+};
+
+const Navbar_links = () => {
 	// Hooks
 	const currentPath = usePathname();
-	const session = useSession();
 
 	const nav_links = [
 		{
@@ -34,81 +51,76 @@ const Navbar = () => {
 
 	return (
 		<>
-			<div className="c-navbar">
-				<div className="c-navbar-inner page_content">
-					<Link href="/" className="c-navbar-logo">
-						<FaBug />
+			<div className="c-navbar-links">
+				{nav_links.map((link, index) => (
+					<Link
+						key={76518 + index}
+						href={link.href}
+						className={`c-navbar-link font-medium text-zinc-500 hover:text-zinc-800 transition-all ${
+							currentPath === link.href ? "text-zinc-800" : ""
+						}`}
+					>
+						{link.name}
 					</Link>
-					<div className="c-navbar-links">
-						{nav_links.map((link, index) => (
-							<Link
-								key={76518 + index}
-								href={link.href}
-								className={`c-navbar-link font-medium text-zinc-500 hover:text-zinc-800 transition-all ${
-									currentPath === link.href
-										? "text-zinc-800"
-										: ""
-								}`}
-							>
-								{link.name}
-							</Link>
-						))}
-						{session.status === "authenticated" ? (
-							<>
-								<DropdownMenu.Root>
-									<DropdownMenu.Trigger>
-										<Avatar
-											radius="full"
-											size="2"
-											className="cursor-pointer"
-											src={session.data?.user?.image!}
-											fallback={
-												session.data?.user?.name?.charAt(
-													1
-												)!
-											}
-										/>
-									</DropdownMenu.Trigger>
-									<DropdownMenu.Content>
-										<DropdownMenu.Label>
-											<Text color="gray">
-												{session.data?.user?.email}
-											</Text>
-										</DropdownMenu.Label>
-										<DropdownMenu.Separator />
-										<DropdownMenu.Label>
-											<Link
-												href="/api/auth/signout"
-												className="!w-full"
-											>
-												<Button
-													color="red"
-													className="!cursor-pointer !w-full"
-												>
-													Logout
-												</Button>
-											</Link>
-										</DropdownMenu.Label>
-									</DropdownMenu.Content>
-								</DropdownMenu.Root>
-							</>
-						) : (
-							session.status === "unauthenticated" && (
-								<>
-									<Link href="/api/auth/signin">
-										<Button
-											color="blue"
-											className="!cursor-pointer"
-										>
-											Login
-										</Button>
-									</Link>
-								</>
-							)
-						)}
-					</div>
-				</div>
+				))}
 			</div>
+		</>
+	);
+};
+
+const Dropdown_menu = () => {
+	// Hooks
+	const session = useSession();
+
+	return (
+		<>
+			{session.status === "authenticated" ? (
+				<>
+					<DropdownMenu.Root>
+						<DropdownMenu.Trigger>
+							<Avatar
+								radius="full"
+								size="2"
+								className="cursor-pointer"
+								src={session.data?.user?.image!}
+								fallback={session.data?.user?.name?.charAt(1)!}
+								referrerPolicy="no-referrer"
+							/>
+						</DropdownMenu.Trigger>
+						<DropdownMenu.Content>
+							<DropdownMenu.Label>
+								<Text color="gray" weight="medium">
+									{session.data?.user?.email}
+								</Text>
+							</DropdownMenu.Label>
+							<DropdownMenu.Separator />
+							<DropdownMenu.Label>
+								<Link
+									href="/api/auth/signout"
+									className="!w-full"
+								>
+									<Button
+										color="red"
+										className="!cursor-pointer !w-full"
+									>
+										Logout
+									</Button>
+								</Link>
+							</DropdownMenu.Label>
+						</DropdownMenu.Content>
+					</DropdownMenu.Root>
+				</>
+			) : (
+				session.status === "unauthenticated" && (
+					<>
+						<Link href="/api/auth/signin">
+							<Button color="blue" className="!cursor-pointer">
+								Login
+							</Button>
+						</Link>
+					</>
+				)
+			)}
 		</>
 	);
 };
